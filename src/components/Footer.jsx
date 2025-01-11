2f62ef6a1ba0bbdfd7b84810facd477dadb94495
Davin-01 <moengadavid90@gmail.com>
2025-01-11 21:43:16 +0300

    vercel.json file into the root directory

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <footer className="bg-primary text-white py-6">
         <div className="container mx-auto text-center">
-          <p className="text-sm">&copy; {new Date().getFullYear()} Small Store. All Rights Reserved.</p>
+          <p className="text-sm">&copy; {new Date().getFullYear()} Precious Star. All Rights Reserved.</p>
           <div className="mt-4">
             <a href="#" className="mx-2 hover:text-accent">Contact Us</a>
             <a href="#" className="mx-2 hover:text-accent">Privacy Policy</a>
